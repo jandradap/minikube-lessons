a58754175044c6b538b8aa59968a6f662e0e9dc7
Jorge Andrada <jandradap@gmail.com>
2018-03-15 07:33:28 +0100

    test

---
+++ image/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# -*- ENCODING: UTF-8 -*-
+#
+# ------------------------------------------------------------------
+# [Jorge Andrada Prieto] [jandradap@gmail.com]
+# Title:
+# Description:
+# ------------------------------------------------------------------
+#
+
+#debug
+# set -x
+# trap read debug
+
+sed -i "s/MODIFICAR/$HOSTNAME/g" /usr/share/nginx/html/index.html
+nginx -g daemon off;
